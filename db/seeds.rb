469b68cfe4825e4cfd06182bf41b9a50e675ffbc
sandralouise <sandra.adamshallie@gmail.com>
2016-05-22 20:51:39 +0000

    Update gemfile to seed data and post index controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+50.times { |i| Post.create(title: "Post #{i}", content: BetterLorem.p(5, false, false)) }
